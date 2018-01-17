0c5560786d160a9bfb4656fb06c6c92797c63de2
Abid Munir Malik <AbidMunirMalik@gmail.com>
2018-01-17 08:47:32 -0500

    Add files via upload

---
+++ 1-Comments.py
@@ -0,0 +1,14 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+# Comments in Python start with the hash character.
+# A comment may appear at the start of a line or following whitespace or code, but not within a string literal.
+# A hash character within a string literal is just a hash character.
+
+# this is the first comment
+name = "Abid Malik"  # and this is the second comment
+          # ... Comments can start even in the middle
+fullname = "# Abid Munir Malik <-- This is not a comment because it's inside quotes."
+print(fullname)
+
+#Now let's run our python file
+#abidmalik$ python3 Comments.py
+# Abid Munir Malik This is not a comment because it's inside quotes.
