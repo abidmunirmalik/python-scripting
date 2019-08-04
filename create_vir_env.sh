7bc4c75c9ae3f687e77f59e8cb0f6177feebf7ed
Abid Malik <abidmunirmalik@gmail.com>
2019-08-04 10:59:31 +0500

    added by developer 1

---
+++ create_vir_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script to create Python virtual envrionment
+# by Developer 1
+#
+i
+python3 -m venv ~/.devops
+source ~/.devops/bin/activate
+echo "Virtual Environment Created!"
+
+exit 0
+ 
+
