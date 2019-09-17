524c9e4e12df741406dfe83e0ae38b6dfeb564fc
Abid Malik <AbidMunirMalik@gmail.com>
2019-09-17 09:10:41 +0500

    Update 1-Comments.py

--- 1-Comments.py
+++ 1-Comments.py
@@ -1,13 +1,12 @@
-# Comments in Python start with the hash character.
-# A comment may appear at the start of a line or following whitespace or code, but not within a string literal.
-# A hash character within a string literal is just a hash character.
+# PYTHON COMMENTS
 
-# this is the first comment
-name = "Abid Malik"  # and this is the second comment
-          # ... Comments can start even in the middle
-fullname = "# Abid Munir Malik <-- This is not a comment because it's inside quotes."
-print(fullname)
+# This is single-line comment in python
 
-#Now let's run our python file
-#abidmalik$ python3 Comments.py
-# Abid Munir Malik This is not a comment because it's inside quotes.
+"""
+This is not a block comment,
+but it will still work when you really need
+for some lines of code to not execute.
+"""
+
+# Above multi-line string can act as comment but there is no such thing of 'block comment' in python
+
